b7b07920ddf85c374ea5885088cfe0c2fea7725b
Mamun <beparimamun708@gmail.com>
2026-03-18 21:14:04 +0600

    Fix: Separated asgi.py from settings.py and configuared production variables

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,26 @@ load_dotenv()  # Load environment variables from .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
 
 # Quick-start development settings - unsuitable for production
 
